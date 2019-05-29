5047f9c3d467171b3401a9155ff7f9f1d9a84e8f
chirag choudha <chirag.choudha@volkswagen.co.in>
2019-05-29 10:33:15 +0530

    Skip Credential Verification

--- terraform-aws-vpc/aws.tf
+++ terraform-aws-vpc/aws.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    skip_credentials_validation = true
+    rskip_requesting_account_id = true
     region = "${var.aws_region}"
 }
