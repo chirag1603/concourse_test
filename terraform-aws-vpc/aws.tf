7f1b945dc47040a8018e2f9e7b980e914763c614
chirag choudha <chirag.choudha@volkswagen.co.in>
2019-05-29 10:34:46 +0530

    Correcting Typo

--- terraform-aws-vpc/aws.tf
+++ terraform-aws-vpc/aws.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    rskip_requesting_account_id = true
+    skip_requesting_account_id = true
     region = "${var.aws_region}"
 }
